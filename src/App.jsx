07b8ac0ca23ef891e92f9264d39ab517966de193
Piyush Kukade <piyushkukade1@gmail.com>
2023-10-12 23:40:01 +0530

    C3: Button

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import Styles from "../Navbar/Navbar.module.css";
+import QtifyLogo from "./QtifyLogo.svg";
+import { AiOutlineSearch } from "react-icons/ai";
+const Navbar = () => {
+  return (
+    <div className={Styles.container}>
+      <img src={QtifyLogo} alt="qtify" className={Styles.imageLogo}></img>
+      <form>
+        <input
+          className={Styles.input}
+          type="text"
+          placeholder="Search a album of your choice"
+        ></input>
+        <button className={Styles.button} type="submit">
+          <AiOutlineSearch />
+        </button>
+      </form>
+
+      <div className={Styles.feedbackContainer}>
+        <button type="button" className={Styles.feedback}>
+          Give Feedback
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
