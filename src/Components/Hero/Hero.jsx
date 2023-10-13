bd77db8caeceeb77300a486fb8359e40b6f93cce
Piyush Kukade <piyushkukade1@gmail.com>
2023-10-13 21:50:41 +0530

    C5: Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,18 @@
+import Styles from "./Hero.module.css";
+import Headphones from "../../assets/Headphones.svg";
+
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className={Styles.heroContainer}>
+      <div>
+        <h1 className={Styles.firstLine}>100 Thousand Songs, ad-free</h1>
+        <h1 className={Styles.secondLine}>Over thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={Headphones} alt="headphones"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,16 +5,14 @@ const Navbar = () => {
   return (
     <div className={Styles.container}>
       <img src={QtifyLogo} alt="qtify" className={Styles.imageLogo}></img>
-      <form>
-        <input
-          className={Styles.input}
-          type="text"
-          placeholder="Search a album of your choice"
-        ></input>
-        <button className={Styles.button} type="submit">
-          <AiOutlineSearch />
-        </button>
-      </form>
+      <input
+        className={Styles.input}
+        type="text"
+        placeholder="Search a album of your choice"
+      ></input>
+      <button className={Styles.button} type="button">
+        <AiOutlineSearch />
+      </button>
 
       <div className={Styles.feedbackContainer}>
         <button type="button" className={Styles.feedback}>
